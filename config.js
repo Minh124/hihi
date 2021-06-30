d8f605d2921d25aca2521959da0627cd6f943025
Minh124 <86700098+Minh124@users.noreply.github.com>
2021-06-30 11:46:36 +0700

    PNM124
    Hihi

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
     introTitle: 'Hello !',
     introDesc: `Thích thì thích không thích phải thích,
-
- 
-
     btnIntro: 'hihi',
-    title: 'câụ thích mình phải không? 😙',
-    desc: 'haha ',
-    btnYes: 'Tớ thích cậu <33',
-    btnNo: 'Không, cậu ạ  :3',
-    question:'Trên thế giới hơn 7 tỉ người sao lại thích tớ <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu không yêu phải yêu <33333333',
+    title: 'cậu thích mình phải không? ',
+    desc: 'haha',
+    btnYes: 'Tớ thích cậu:33',
+    btnNo: ' Không, cậu ạ:v',
+    question: 'Tại sao vậy?,
+    btnReply: ' Gửi cho tớ ',
+    Reply: ' tại cậu đáng iu :3',
+    
+}   
+    
+   
     
    
    
 
-}
+
